7327c9292ac463f85825d441baf5204d778b879c
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-02-23 06:13:14 +0530

    config the mdx and base_scss and img domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-// const path = require("path");
 import path from 'path';
 import { fileURLToPath } from 'url';
+import withMDX from '@next/mdx';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -14,13 +13,22 @@ const nextConfig = {
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "./base.scss";`,
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    domains: ['img.freepik.com'], // Add the domain for your images
+  },
 };
 
-export default nextConfig;
+export default withMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})(nextConfig);
