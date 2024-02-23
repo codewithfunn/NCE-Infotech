d71c55baa3e856e9d00c31b44ca9cd3de20f75a9
jaspreet_suri <160199423+SKTECHZ@users.noreply.github.com>
2024-02-23 06:21:54 +0530

    creating home page.

---
+++ components/Banner/index.js
@@ -0,0 +1,152 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import required modules
+import { Autoplay,Pagination, Navigation } from "swiper/modules";
+import Link from "next/link";
+import Image from "next/image";
+import styles from './styles.module.scss'
+import { Typewriter } from "react-simple-typewriter";
+export const BannerWithoutSwiper = ({ data }) => {
+
+    const handleType = (count) => {
+      // access word count number
+      console.log(count);
+    };
+  
+    const handleDone = () => {
+      console.log(`Done after 5 loops!`);
+    };
+    return (
+      <div
+        className={styles.__bannerWithoutSwiperConatainer}
+        style={{ flexDirection: data?.bannerImage?.reverse ? "row-reverse" : "row" }}
+      >
+        <div className={styles.contentContainer}>
+          {data?.title?.enableTypewriter ? (
+            <h1>
+              <Typewriter
+                words={data?.title?.typewriter?.words}
+                loop={data?.title?.typewriter?.loop}
+                cursor
+                cursorStyle={data?.title?.typewriter?.cursorStyle}
+                typeSpeed={data?.title?.typewriter?.typeSpeed}
+                deleteSpeed={data?.title?.typewriter?.deleteSpeed}
+                delaySpeed={data?.title?.typewriter?.delaySpeed}
+                onLoopDone={handleDone}
+                onType={handleType}
+              />
+            </h1>
+          ) : (
+            <h1>{data?.title?.content}</h1>
+          )}
+          {data.content.heading && <h2>{data?.content?.heading}</h2>}
+          {data?.content?.paragraph.map((item, index) => {
+            return item.p ? (
+              <p key={index}>
+                {" "}
+                {item.p} <Link href={item.link.href}>{item.link.content}</Link>
+              </p>
+            ) : (
+              <p key={index}>{item}</p>
+            );
+          })}
+  
+          <span className={`${styles.__btn} flex flex-row items-center`}>
+            <Link href={data?.button?.link} className="border-2 shadow-sm text-gray-500 border-gray-400 rounded-md ">
+              <button>{data?.button?.content}</button>
+            </Link>
+          </span>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src={data?.bannerImage?.url} alt={data?.bannerImage?.alt} width={600} height={600} />
+        </div>
+      </div>
+    );
+  };
+const Index = ({bannerData1, options}) => {
+    const bannerData =[
+        {
+          title: "Better Solutions For Your Business",
+          content: "Software Development Company, an Amazing Doorway to Rise Your Business.",
+          button:{
+            content: "Get In touch",
+            link: "/",
+          },
+          sideImage:{
+            url: "/image/Banner/homeBanner-2.png",
+            alt: "BannerImg",
+          }
+        },
+        {
+          title: "Better Solutions For Your Business",
+          content: "Software Development Company, an Amazing Doorway to Rise Your Business.",
+          button:{
+            content: "Get In touch",
+            link: "/",
+          },
+          sideImage:{
+            url: "/image/Banner/homeBanner-2.png",
+            alt: "BannerImg",
+          }
+        },
+        {
+          title: "Better Solutions For Your Business",
+          content: "Software Development Company, an Amazing Doorway to Rise Your Business.",
+          button:{
+            content: "Get In touch",
+            link: "/",
+          },
+          sideImage:{
+            url: "/image/Banner/homeBanner-2.png",
+            alt: "BannerImg",
+          }
+        },
+      ]
+  return (
+    <div className={styles.__bannerContainer}>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay,Pagination, Navigation]}
+        className={`mySwiper ${styles.__bannerSwiper}`}
+      >
+        {
+          bannerData.map((item,index)=>{
+            return (
+              <SwiperSlide key={index} className={styles.swiperSlide}>
+                <div className={styles.__bannerContent}>
+               <div>
+                    <h2>{item?.title}</h2>
+                    <p>{item?.content}</p>
+                    <button>
+                        <Link href={item?.button?.link}>{item?.button?.content}</Link>
+                    </button>
+               </div>
+               <div>
+                <Image src={item?.sideImage?.url} alt={item?.sideImage?.alt} width={500} height={500} />
+               </div>
+                </div>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </div>
+  );
+};
+
+export default Index;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,275 @@
 import Image from "next/image";
-
-
-export default function Home() {
+import Banner from "@/components/Banner";
+import styles from "@/styles/home.module.scss";
+import Link from "next/link";
+import { IoCheckmarkDone } from "react-icons/io5";
+import { GiMeshBall } from "react-icons/gi";
+import { useRouter } from "next/router";
+export const AboutUs = () => {
+  const data = {
+    heading: "ABOUT US",
+    content: {
+      heading:
+        "Work with a software company that develops products & platforms that elevate your customers’ experience, earn their loyalty & grow your business.",
+      links: [
+        {
+          icon: <IoCheckmarkDone />,
+          content: "Experience Design",
+        },
+        {
+          icon: <IoCheckmarkDone />,
+          content: "Ecommerce Development",
+        },
+        {
+          icon: <IoCheckmarkDone />,
+          content: "Digital Experience Platform",
+        },
+        {
+          icon: <IoCheckmarkDone />,
+          content: "Data Analytics",
+        },
+        {
+          icon: <IoCheckmarkDone />,
+          content: "Digital Transformation",
+        },
+      ],
+    },
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-    >
-      
-    </main>
+    <div className={styles.about__us}>
+      <div className={styles.about__us__heading}>
+        <h2>{data?.heading}</h2>
+      </div>
+      <div className={styles.about__us__content}>
+        <div className={styles.about__us__content__text}>
+          <p>{data?.content?.heading}</p>
+          <ul>
+            {data?.content?.links?.map((item, index) => (
+              <li key={index}>
+                <span>{item?.icon}</span>
+                <span>{item?.content}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.about__us__content__btnContainer}>
+          <span>Software, App,and Website Development Agency Services</span>
+          <button>
+            <Link href={"/"}>Learn More</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export const TransformativePlatform = () => {
+  const data = {
+    heading: "Expand & Evolve Your Business With Our Transformative Platforms",
+    subheading:
+      "Here, NCE Infotech comes to the picture with a confidence of leading you to success! We provide range of services starting from Website Designing Services and Website Development Services including with Ecommerce Solutions, SEO, Open Source Development, iPhone Development, PHP MYSQL Development to make sure that we place you in the prime position.",
+    img: {
+      url: "/image/Banner/homeBanner-1.png",
+      alt: "Transformative_img",
+    },
+  };
+  return (
+    <div className={styles.TransformativePlatform__container}>
+      <div className={styles.TransformativePlatform}>
+        <div className={styles.TransformativePlatform__content}>
+          <h2>{data?.heading}</h2>
+          <p>{data?.subheading}</p>
+        </div>
+        <div className={styles.TransformativePlatform__img}>
+          <Image
+            src={data.img.url}
+            alt={data.img.alt}
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
+      <OurOffers />
+    </div>
+  );
+};
+export const OurOffers = () => {
+  const router = useRouter();
+  const data = {
+    heading: "WHAT WE OFFER",
+    subheading:
+      "We are a dedicated team of software professionals with expertise in delivering a wide range of services, including Software Solutions, IT Support, Web Development, Design and Digital Marketing. Our primary objective is to leverage state-of-the-art technologies to foster the growth and success of present and future business goals.",
+    cards: [
+      {
+        content: {
+          heading: "Full Stack Development",
+          subheading:
+            "We have amassed unmatched expertise dealing with both front-end and back-end technologies for full-stack development.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "Mobile App Development",
+          subheading:
+            "We build superior mobile apps or revamp your existing mobile application with customizable project modifications.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "Web Application Development",
+          subheading:
+            "Our web design and development company provides top-tier web design services aimed at enhancing our clients' online visibility.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "Product Development",
+          subheading:
+            "Develop your custom digital product and unique business offering with our robust and reliable Product development services.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "ERP Solution",
+          subheading:
+            "Enterprise Resource Planning (ERP) is a software system that integrates and manages core business processes, such as finance, supply chain, operations, reporting, manufacturing, CRM, and human resource activities, within a single unified platform.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "SALESFORCE IMPLEMENTATION ",
+          subheading:
+            "The best-in-class Salesforce customization and implementation services can help businesses gain a tactical advantage.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "Cloud ERP IMPLEMENTATION ",
+          subheading:
+            "Automate Sales, Manufacturing, and Inventory in One App.  The best-in-class CRM customization and implementation services can help businesses gain a tactical advantage. ",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+      {
+        content: {
+          heading: "ERP Solution",
+          subheading:
+            "Enterprise Resource Planning (ERP) is a software system that integrates and manages core business processes, such as finance, supply chain, operations, reporting, manufacturing, CRM, and human resource activities, within a single unified platform.",
+          icon: <GiMeshBall />,
+        },
+        link: "/",
+      },
+    ],
+  };
+  return (
+    <div className={styles.ourOffers}>
+      <div className={styles.ourOffers__heading}>
+        <h2>{data?.heading}</h2>
+        <p>{data?.subheading}</p>
+      </div>
+      <ul className={styles.ourOffers__cards}>
+        {data?.cards?.map((item, index) => (
+          <li key={index} className="shadow-md hover:shadow-2xl cursor-pointer " onClick={() => router.push(item?.link)}>
+            <span className={styles.ourOffers__cards__icon}>{item?.content?.icon}</span>
+            <span className={styles.ourOffers__cards__heading}>{item?.content?.heading}</span>
+            <span className={styles.ourOffers__cards__subheading}>{item?.content?.subheading}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export const CallToAction = () => {
+  const data = {
+    heading: "Call To Action",
+    subheading: "Got a Software or App Development Project in Mind?",
+    button: {
+      content: "Get Started Now",
+      link: "/",
+    }
+  };
+  return (
+    <div className={styles.callToActionContainer}>
+    <div className={styles.callToAction}>
+      <div className={styles.callToAction__heading}>
+        <h2>{data?.heading}</h2>
+        <p>{data?.subheading}</p>
+      </div>
+      <div className={styles.callToAction__button}>
+        <Link href={data?.button?.link}>{data?.button?.content}</Link>
+      </div>
+    </div>
+    </div>
+  );
+}
+export default function Home() {
+  const bannerData = [
+    {
+      title: "Better Solutions For Your Business",
+      content:
+        "Software Development Company, an Amazing Doorway to Rise Your Business.",
+      button: {
+        content: "Get In touch",
+        link: "/",
+      },
+      sideImage: {
+        url: "/image/Banner/homeBanner-2.png",
+        alt: "BannerImg",
+      },
+    },
+    {
+      title: "Better Solutions For Your Business",
+      content:
+        "Software Development Company, an Amazing Doorway to Rise Your Business.",
+      button: {
+        content: "Get In touch",
+        link: "/",
+      },
+      sideImage: {
+        url: "/image/Banner/homeBanner-2.png",
+        alt: "BannerImg",
+      },
+    },
+    {
+      title: "Better Solutions For Your Business",
+      content:
+        "Software Development Company, an Amazing Doorway to Rise Your Business.",
+      button: {
+        content: "Get In touch",
+        link: "/",
+      },
+      sideImage: {
+        url: "/image/Banner/homeBanner-2.png",
+        alt: "BannerImg",
+      },
+    },
+  ];
+  return (
+    <div>
+      <Banner
+        bannerData={bannerData}
+        options={[
+          { title: true },
+          { content: true },
+          { button: true },
+          { sideImage: true },
+        ]}
+      />
+      <AboutUs />
+      <TransformativePlatform />
+<CallToAction/>
+    </div>
   );
 }
